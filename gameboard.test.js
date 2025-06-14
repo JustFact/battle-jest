a6935d927329539a412ee6fb221de48a8a401632
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2025-06-15 05:22:05 +0530

    Test the ship placement function

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,4 +1,5 @@
 const Gameboard = require("./gameboard");
+const Ship = require("./ship");
 
 test("Testing gameboard", () => {
   let testValue = [
@@ -26,7 +27,7 @@ test("Test missed attacks", () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, "x", 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, "x", 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, "x"],
   ];
@@ -34,5 +35,48 @@ test("Test missed attacks", () => {
   gameboard.receiveAttack(0, 0);
   gameboard.receiveAttack(5, 5);
   gameboard.receiveAttack(9, 9);
+  gameboard.receiveAttack(7, 4);
   expect(gameboard.getBoard()).toStrictEqual(testValue);
+});
+
+test("Test ship placement", () => {
+  let gameboard = Gameboard();
+  let ship = Ship(4);
+  let expectedValue = [
+    [ship, ship, ship, ship, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  expect(gameboard.placeShip(0, 0, ship)).toBe(true);
+  expect(gameboard.getBoard()).toStrictEqual(expectedValue);
+
+  //Following ship should not be placed
+  ship.changeOrientation();
+  expect(gameboard.placeShip(0, 0, ship)).toBe(false);
+  expect(gameboard.getBoard()).toStrictEqual(expectedValue);
+
+  //Ship should be placed vertically
+  let ship2 = Ship(2);
+  ship2.changeOrientation();
+  let expectedValue2 = [
+    [ship, ship, ship, ship, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, ship2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, ship2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  expect(gameboard.placeShip(3, 2, ship2)).toBe(true);
+  expect(gameboard.getBoard()).toStrictEqual(expectedValue2);
 });
