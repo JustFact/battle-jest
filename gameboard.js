487a9e1a509a68f839632925cd8b57cefdc136b8
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2025-06-15 05:18:32 +0530

    Add ship placement logic
    The ship placement function will check and place
    the ships at the specified coordinates. It places
    the ship and then return 'true' otherwise if ship
    cannot be placed then it returns 'false'.

--- gameboard.js
+++ gameboard.js
@@ -10,9 +10,47 @@ const Gameboard = () => {
   };
 
   const getBoard = () => board;
+
+  const placeShip = (x, y, ship) => {
+    let ori = ship.getOrientation();
+    let len = ship.getLength();
+    let canBePlaced = true; //checks if the ship can be placed
+    //check loop
+    for (let i = 0; i < len; i++) {
+      if (ori === "X") {
+        if (board[x][y + i] !== 0) {
+          canBePlaced = canBePlaced && false;
+        } else {
+          canBePlaced = canBePlaced && true;
+        }
+      } else if (ori === "Y") {
+        if (board[x + i][y] !== 0) {
+          canBePlaced = canBePlaced && false;
+        } else {
+          canBePlaced = canBePlaced && true;
+        }
+      }
+    }
+
+    //placing ship
+    for (let i = 0; i < len; i++) {
+      if (ori === "X") {
+        if (canBePlaced) {
+          board[x][y + i] = ship;
+        }
+      } else if (ori === "Y") {
+        if (canBePlaced) {
+          board[x + i][y] = ship;
+        }
+      }
+    }
+    return canBePlaced;
+  };
+
   return {
     receiveAttack,
     getBoard,
+    placeShip,
   };
 };
 
